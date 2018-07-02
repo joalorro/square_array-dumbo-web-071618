3e9ce7db94f757862f45df435a80bf90ccaf70b5
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 15:15:25 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,11 @@ arr = [1,2,3,4]
 
 def square_array(array)
   # your code here
-  array.each do |num|
+  newArr array.each do |num|
     puts num ** 2 
   end
+  
+  return newArr
 end
 
 puts square_array(arr)
